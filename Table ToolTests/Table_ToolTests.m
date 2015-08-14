951b12a2a65ac101220ba9f170a34b8f5a6a5e12
Andreas Aigner <aigi.heh@gmx.at>
2015-08-14 12:27:25 +0200

    Update Test and Test Documents
    several added tests and documents for testing

--- Table ToolTests/Table_ToolTests.m
+++ Table ToolTests/Table_ToolTests.m
@@ -10,6 +10,7 @@
 #import <XCTest/XCTest.h>
 #import "CSVReader.h"
 #import "CSVConfiguration.h"
+#import "CSVHeuristic.h"
 
 @interface Table_ToolTests : XCTestCase {
     int count;
@@ -27,7 +28,6 @@
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
@@ -130,7 +130,7 @@
     XCTAssert(YES, @"Pass");
 }
 
-- (void)testReadCSVFileShouldGetErrorCode1 {
+- (void)testReadCSVFileWithInvalidEncoding_ShouldGetErrorCode1 {
     NSURL *testFileURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"Reading Test Documents/invalid-encoding" withExtension:@"csv"];
     NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileURL];
     CSVReader *reader = [[CSVReader alloc]initWithData:testData configuration:config];
@@ -140,7 +140,7 @@
         if(error) break;
         XCTAssertNotNil(line, "CSVReader should return object.");
     }
-    XCTAssertTrue(error.code == 1, "Returned wrong error code");
+    XCTAssertEqual(error.code, 1, "Returned wrong error code");
     XCTAssert(YES, @"Pass");
 }
 
@@ -153,27 +153,261 @@
         NSArray *line = [reader readLineWithError:&error];
         if(error) break;
         XCTAssertNil(line, "CSVReader should not return object.");
-        }
-    XCTAssertTrue(error.code == 2, "Returned wrong error code");
+    }
+    XCTAssertEqual(error.code, 2, "Returned wrong error code");
     XCTAssert(YES, @"Pass");
 }
 
-- (void)testReadCSVFileShouldGetErrorCode3 {
-    NSURL *testFileURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"Reading Test Documents/invalid-quote" withExtension:@"csv"];
+- (void)testReadCSVFileWithMissingBackslashBeforeValue_ShouldGetErrorCode2 {
+    NSURL *testFileURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"Reading Test Documents/missing-backslash-beforeValue" withExtension:@"csv"];
     NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileURL];
-    config.quoteCharacter = @"";
+    config.escapeCharacter = @"\\";
     CSVReader *reader = [[CSVReader alloc]initWithData:testData configuration:config];
     NSError *error = nil;
     while(!reader.isAtEnd){
         NSArray *line = [reader readLineWithError:&error];
         if(error) break;
-        XCTAssertNotNil(line, "CSVReader should return object.");
+        XCTAssertNil(line, "CSVReader should not return object.");
     }
-    XCTAssertTrue(error.code == 3, "Returned wrong error code");
+    XCTAssertEqual(error.code, 2, "Returned wrong error code");
     XCTAssert(YES, @"Pass");
 }
 
-- (void)testReadUnquotedCSVFile {
+- (void)testReadCSVFileWithMissingBackslashAfterValue_ShouldGetErrorCode2 {
+    NSURL *testFileURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"Reading Test Documents/missing-backslash-afterValue" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileURL];
+    config.escapeCharacter = @"\\";
+    CSVReader *reader = [[CSVReader alloc]initWithData:testData configuration:config];
+    NSError *error = nil;
+    while(!reader.isAtEnd){
+        NSArray *line = [reader readLineWithError:&error];
+        if(error) break;
+        XCTAssertNil(line, "CSVReader should not return object.");
+    }
+    XCTAssertEqual(error.code, 2, "Returned wrong error code");
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testReadCSVFileWithMissingValueForBackslashInquote_ShouldGetErrorCode2 {
+    NSURL *testFileURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"Reading Test Documents/missing-value-for-backslash-inquote" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileURL];
+    config.escapeCharacter = @"\\";
+    CSVReader *reader = [[CSVReader alloc]initWithData:testData configuration:config];
+    NSError *error = nil;
+    while(!reader.isAtEnd){
+        NSArray *line = [reader readLineWithError:&error];
+        if(error) break;
+        XCTAssertNil(line, "CSVReader should not return object.");
+    }
+    XCTAssertEqual(error.code, 2, "Returned wrong error code");
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testReadCSVFileWithMissingValueForeBackslashBeforeValue_ShouldGetErrorCode2 {
+    NSURL *testFileURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"Reading Test Documents/missing-value-for-backslash" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileURL];
+    config.escapeCharacter = @"\\";
+    CSVReader *reader = [[CSVReader alloc]initWithData:testData configuration:config];
+    NSError *error = nil;
+    while(!reader.isAtEnd){
+        NSArray *line = [reader readLineWithError:&error];
+        if(error) break;
+        XCTAssertNil(line, "CSVReader should not return object.");
+    }
+    XCTAssertEqual(error.code, 2, "Returned wrong error code");
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testReadCSVFileWithQuoteInUnquotedValue_ShouldGetErrorCode3 {
+    NSURL *testFileURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"Reading Test Documents/quote-in-unquoted-value" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileURL];
+    CSVReader *reader = [[CSVReader alloc]initWithData:testData configuration:config];
+    NSError *error = nil;
+    while(!reader.isAtEnd){
+        NSArray *line = [reader readLineWithError:&error];
+        if(error) break;
+        XCTAssertNil(line, "CSVReader should not return object.");
+    }
+    XCTAssertEqual(error.code, 3, "Returned wrong error code");
+    XCTAssert(YES, @"Pass");
+}
+
+//For config descriptions see CSVHeuristic.m
+- (void)testHeuristicConfig1 {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-config1" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual([config.columnSeparator characterAtIndex:0], ',', "Column Separator should be ','.");
+    XCTAssertEqual([config.decimalMark characterAtIndex:0], '.', "Decimal Mark should be '.'.");
+    XCTAssertEqual([config.quoteCharacter characterAtIndex:0], '\"', "Quote should be enabled.");
+    XCTAssertTrue(config.firstRowAsHeader, "First row should be header.");
+    XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\"', "Escape character should be '\"'.");
+}
+
+//For config descriptions see CSVHeuristic.m
+- (void)testHeuristicConfig2 {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-config2" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual([config.columnSeparator characterAtIndex:0], ';', "Column Separator should be ';'.");
+    XCTAssertEqual([config.decimalMark characterAtIndex:0], ',', "Decimal Mark should be ','.");
+    XCTAssertEqual([config.quoteCharacter characterAtIndex:0], '\"', "Quote should be enabled.");
+    XCTAssertTrue(config.firstRowAsHeader, "First row should be header.");
+    XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\"', "Escape character should be '\"'.");
+}
+
+//For config descriptions see CSVHeuristic.m
+- (void)testHeuristicConfig3 {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-config3" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual([config.columnSeparator characterAtIndex:0], '\t', "Column Separator should be '\t'.");
+    XCTAssertEqual([config.decimalMark characterAtIndex:0], '.', "Decimal Mark should be '.'.");
+    XCTAssertEqual([config.quoteCharacter characterAtIndex:0], '\"', "Quote should be enabled.");
+    XCTAssertTrue(config.firstRowAsHeader, "First row should be header.");
+    XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\"', "Escape character should be '\"'.");
+}
+
+//For config descriptions see CSVHeuristic.m
+- (void)testHeuristicConfig4 {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-config4" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual([config.columnSeparator characterAtIndex:0], '\t', "Column Separator should be '\t'.");
+    XCTAssertEqual([config.decimalMark characterAtIndex:0], ',', "Decimal Mark should be ','.");
+    XCTAssertEqual([config.quoteCharacter characterAtIndex:0], '\"', "Quote should be enabled.");
+    XCTAssertTrue(config.firstRowAsHeader, "First row should be header.");
+    XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\"', "Escape character should be '\"'.");
+}
+
+//For config descriptions see CSVHeuristic.m
+- (void)testHeuristicConfig5 {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-config5" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual([config.columnSeparator characterAtIndex:0], ';', "Column Separator should be ';'.");
+    XCTAssertEqual([config.decimalMark characterAtIndex:0], ',', "Decimal Mark should be ','.");
+    XCTAssertEqual([config.quoteCharacter characterAtIndex:0], '\"', "Quote should be enabled.");
+    XCTAssertTrue(config.firstRowAsHeader, "First row should be header.");
+    XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\\', "Escape character should be '\\'.");
+}
+
+//For config descriptions see CSVHeuristic.m
+- (void)testHeuristicConfig6 {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-config6" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual([config.columnSeparator characterAtIndex:0], ',', "Column Separator should be ','.");
+    XCTAssertEqual([config.decimalMark characterAtIndex:0], '.', "Decimal Mark should be '.'.");
+    XCTAssertTrue([config.quoteCharacter isEqualToString:@""], "Quote should be disabled.");
+    XCTAssertTrue(config.firstRowAsHeader, "First row should be header.");
+    XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\"', "Escape character should be '\"'.");
+}
+
+//For config descriptions see CSVHeuristic.m
+- (void)testHeuristicConfig7 {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-config7" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual([config.columnSeparator characterAtIndex:0], ';', "Column Separator should be ';'.");
+    XCTAssertEqual([config.decimalMark characterAtIndex:0], ',', "Decimal Mark should be ','.");
+    XCTAssertTrue([config.quoteCharacter isEqualToString:@""], "Quote should be disabled.");
+    XCTAssertTrue(config.firstRowAsHeader, "First row should be header.");
+    XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\"', "Escape character should be '\"'.");
+}
+
+//For config descriptions see CSVHeuristic.m
+- (void)testHeuristicConfig8 {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-config8" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual([config.columnSeparator characterAtIndex:0], '\t', "Column Separator should be '\t'.");
+    XCTAssertEqual([config.decimalMark characterAtIndex:0], '.', "Decimal Mark should be '.'.");
+    XCTAssertTrue([config.quoteCharacter isEqualToString:@""], "Quote should be disabled.");
+    XCTAssertTrue(config.firstRowAsHeader, "First row should be header.");
+    XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\"', "Escape character should be '\"'.");
+}
+
+//For config descriptions see CSVHeuristic.m
+- (void)testHeuristicConfig9 {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-config9" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual([config.columnSeparator characterAtIndex:0], '\t', "Column Separator should be '\t'.");
+    XCTAssertEqual([config.decimalMark characterAtIndex:0], ',', "Decimal Mark should be ','.");
+    XCTAssertTrue([config.quoteCharacter isEqualToString:@""], "Quote should be disabled.");
+    XCTAssertTrue(config.firstRowAsHeader, "First row should be header.");
+    XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\"', "Escape character should be '\"'.");
+}
+
+//For config descriptions see CSVHeuristic.m
+- (void)testHeuristicConfig10 {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-config10" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual([config.columnSeparator characterAtIndex:0], ',', "Column Separator should be ','.");
+    XCTAssertEqual([config.decimalMark characterAtIndex:0], '.', "Decimal Mark should be '.'.");
+    XCTAssertEqual([config.quoteCharacter characterAtIndex:0], '\"', "Quote should be enabled.");
+    XCTAssertTrue(config.firstRowAsHeader, "First row should be header.");
+    XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\\', "Escape character should be '\\'.");
+}
+
+//For config descriptions see CSVHeuristic.m
+- (void)testHeuristicConfig11 {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-config11" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual([config.columnSeparator characterAtIndex:0], ',', "Column Separator should be ','.");
+    XCTAssertEqual([config.decimalMark characterAtIndex:0], ',', "Decimal Mark should be ','.");
+    XCTAssertEqual([config.quoteCharacter characterAtIndex:0], '\"', "Quote should be enabled.");
+    XCTAssertTrue(config.firstRowAsHeader, "First row should be header.");
+    XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\"', "Escape character should be '\"'.");
+}
+
+- (void)testHeuristicNoHeaderBecauseOfNumber {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-first-row-with-number" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertFalse(config.firstRowAsHeader, "First row should not be header");
+}
+
+- (void)testHeuristicNoHeaderBecauseOfShorterLength {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-first-row-one-row-shorter" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertFalse(config.firstRowAsHeader, "First row should not be header");
+}
+
+- (void)testHeuristicNoHeaderBecauseOfLongerLength {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-first-row-longer" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertFalse(config.firstRowAsHeader, "First row should not be header");
+}
+
+- (void)testHeuristicChangeEncodingToWestern {
+    NSURL *testFileUrl = [[NSBundle bundleForClass:[self class]] URLForResource:@"Heuristic Test Documents/heuristic-no-utf-encoding" withExtension:@"csv"];
+    NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileUrl];
+    CSVHeuristic *heuristic = [[CSVHeuristic alloc] initWithData:testData];
+    config = [heuristic calculatePossibleFormat];
+    XCTAssertEqual(config.encoding, NSWindowsCP1252StringEncoding, "Encoding should be Western");
+}
+
+- (void)testPerformanceReaderUnquotedValues {
     NSURL *testFileURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"test-doc-unquoted" withExtension:@"csv"];
     config.quoteCharacter = @"";
     [self measureBlock:^{
@@ -190,7 +424,7 @@
     }];
 }
 
-- (void)testReadQuotedCSVFile {
+- (void)testPerformanceReaderQuotedValues {
     NSURL *testFileURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"test-doc-quoted" withExtension:@"csv"];
     [self measureBlock:^{
         NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileURL];
@@ -204,6 +438,24 @@
         XCTAssertEqual(count, 1002, "Should have read 1002 lines.");
         count = 0;
     }];
+}
+
+- (void)testPerformanceHeuristic {
+    NSURL *testFileURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"test-doc-quoted" withExtension:@"csv"];
+    [self measureBlock:^{
+        for(int i = 0;i < 10; i++){
+            NSData *testData = [[NSData alloc] initWithContentsOfURL:testFileURL];
+            CSVHeuristic *heuristic = [[CSVHeuristic alloc]initWithData:testData];
+            config = [heuristic calculatePossibleFormat];
+            XCTAssertEqual([config.columnSeparator characterAtIndex:0], ',', "Column Separator should be ','.");
+            XCTAssertEqual([config.decimalMark characterAtIndex:0], ',', "Decimal Mark should be ','.");
+            XCTAssertEqual([config.quoteCharacter characterAtIndex:0], '\"', "Quote should be enabled.");
+            XCTAssertEqual(config.firstRowAsHeader, YES, "First row should be header.");
+            XCTAssertEqual([config.escapeCharacter characterAtIndex:0], '\"', "Escape character should be '\"'.");
+            config = nil;
+        }
+    }];
+    
 }
 
 @end
