0cd7b456972410282aa4f07126d1ca98f99627e7
Martin Koehler <martin.jan.koehler@gmail.com>
2017-06-13 17:54:03 +0200

    * added drag and drop support for the document table view * using drag and drop, (single and multiple selections of) table view rows can now be moved * dragged table view rows are now also placed in the NSPasteBoard in textual CSV form (enables dropping into other applications)

---
+++ Table Tool/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  Table Tool
+//
+//  Created by Martin Koehler on 12.06.17.
+//  Copyright (c) 2017 Egger Apps. All rights reserved.
+//
+
+#include <Cocoa/Cocoa.h>
+
+#pragma mark Pasteboard Types
+
+extern NSString *TTRowInternalPboardType;
